e7f8db1d8b3e76e45d2322e18431b844804cc3c9
Vikrom Narula <vikrom.nar@gmail.com>
2025-11-29 08:46:41 +0700

    test: setup skeleton structure for the test cases

--- test.py
+++ test.py
@@ -1,4 +1,21 @@
 import unittest
 
+class MainTest(unittest.TestCase):
+    def test_byte_counts(self):
+        """ Testing the `-c` flag for the `wc` command """
+        pass
+    def test_newline_couts(self):
+        """ Testing the `-l` flag for the `wc` command """
+        pass
+    def test_word_counts(self):
+        """ Testing the `-w` flag for the `wc` command """
+        pass
+    def test_char_counts(self):
+        """ Testing the `-m` flag for the `wc` command """
+        pass
+    def test_file_input(self):
+        """ Testing the file input for the `wc` command """
+        pass
+
 if __name__ == "__main_":
     unittest.main()
