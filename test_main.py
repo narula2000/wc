ed77a6d710cbedcf3c67693b996b2daea3ab383e
Vikrom Narula <vikrom.nar@gmail.com>
2025-11-29 16:03:18 +0700

    lint: add linter and lint

--- test_main.py
+++ test_main.py
@@ -12,35 +12,35 @@ class TestFileInput:
         test_args = ["-l", self.TEST_FILE]
         main(test_args)
         output = capsys.readouterr().out
-        assert "7145 test.txt\n" == output
+        assert output == "7145 test.txt\n"
 
     def test_byte_counts(self, capsys):
         """Testing the `-c` flag for the `wc` command"""
         test_args = ["-c", self.TEST_FILE]
         main(test_args)
         output = capsys.readouterr().out
-        assert "342190 test.txt\n" == output
+        assert output == "342190 test.txt\n"
 
     def test_word_counts(self, capsys):
         """Testing the `-w` flag for the `wc` command"""
         test_args = ["-w", self.TEST_FILE]
         main(test_args)
         output = capsys.readouterr().out
-        assert "58164 test.txt\n" == output
+        assert output == "58164 test.txt\n"
 
     def test_char_counts(self, capsys):
         """Testing the `-m` flag for the `wc` command"""
         test_args = ["-m", self.TEST_FILE]
         main(test_args)
         output = capsys.readouterr().out
-        assert "339292 test.txt\n" == output
+        assert output == "339292 test.txt\n"
 
     def test_file_input(self, capsys):
         """Testing the file input for the `wc` command"""
         test_args = [self.TEST_FILE]
         main(test_args)
         output = capsys.readouterr().out
-        assert "7145  58164  342190 test.txt\n" == output
+        assert output == "7145  58164  342190 test.txt\n"
 
     def test_multiple_file_input(self, capsys):
         """Testing multiple file inputs for the `wc` command"""
