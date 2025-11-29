0b59bcfe880a55c8eeecbf4a3cff657733f7158f
Vikrom Narula <vikrom.nar@gmail.com>
2025-11-29 10:02:44 +0700

    feat: implement count lines flag
    - refactor to code to be more functional

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
 import argparse
 
 
-def main(argv=None):
+def setup_parser(arguments=None):
     parser = argparse.ArgumentParser()
     parser.add_argument("-l", "--lines", action="store_true")
     parser.add_argument("-c", "--bytes", action="store_true")
     parser.add_argument("-w", "--words", action="store_true")
     parser.add_argument("-m", "--chars", action="store_true")
     parser.add_argument("file", nargs="?")  # optional file argument
-    args = parser.parse_args(argv)
+    return parser.parse_args(arguments)
 
-    if args.lines:
-        print("List flag detected")
-    elif args.bytes:
+
+def count_lines(file):
+    with open(file, "r") as txt:
+        lines = txt.readlines()
+
+    return sum((1 for line in lines))
+
+
+def flag_hanlder(arguments):
+    if arguments.lines:
+        line_count = count_lines(arguments.file)
+        print(f"{line_count} {arguments.file}")
+    elif arguments.bytes:
         print("Byte count detected")
-    elif args.words:
+    elif arguments.words:
         print("Word count detected")
-    elif args.chars:
+    elif arguments.chars:
         print("Char count detected")
     else:
         print("No flag")
+
+
+def main(argv=None):
+    arguments = setup_parser(argv)
+    flag_hanlder(arguments)
 
 
 if __name__ == "__main__":
